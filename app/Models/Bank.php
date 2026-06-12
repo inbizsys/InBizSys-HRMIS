b44415e539fbfbff68d67098f9d6dca6a005c56f
Lakshanj97 <116512874+Lakshanj97@users.noreply.github.com>
2026-06-12 11:39:26 +0530

    Add relationship for Bank model

--- app/Models/Bank.php
+++ app/Models/Bank.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Bank extends Model
 {
@@ -23,4 +24,9 @@ class Bank extends Model
         'bank_name',
         'is_active',
     ];
+
+    public function branches(): HasMany
+    {
+        return $this->hasMany(BankBranch::class);
+    }
 }
